050c316b9909a4002c921d6e9bfc9dc4bc7a9bf6
Necrotic999 <l.ilya041105@gmail.com>
2024-11-01 00:26:20 +0200

    add hero section and styles for mobile

---
+++ src/components/Hero.tsx
@@ -0,0 +1,17 @@
+// import Button from "@/components/Button";
+
+const Hero = () => {
+  return (
+    <section className="min-w-80 h-[491px] bg-[url('../images/hero-bg-img-mobile.png')] md:bg-[url('../images/hero-bg-img-tablet.png')] xl:bg-[url('../images/hero-bg-img-desktop.png)] bg-no-repeat bg-cover pt-[15px] pb-[60px] px-[46px]">
+      <h1 className="text-[#7E6D53] text-5xl font-normal leading-[normal] h-[72px] flex justify-center items-center">
+        Nice view
+      </h1>
+      <p className="text-[#7E6D53] text-center w-[172px] font-light leading-[normal] mx-auto h-[48px] flex justify-center items-center">
+        For your comfortable life with your family
+      </p>
+      {/* <Button type="button" className="" /> */}
+    </section>
+  );
+};
+
+export default Hero;
