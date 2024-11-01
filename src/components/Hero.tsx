1ae02d2fc75a889e3c779d935242538307fce4d0
Favixx <bogdandom52@gmail.com>
2024-11-01 10:01:04 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@
 
 const Hero = () => {
   return (
-    <section className="min-w-80 h-[491px] bg-[url('../images/hero-bg-img-mobile.png')] md:bg-[url('../images/hero-bg-img-tablet.png')] xl:bg-[url('../images/hero-bg-img-desktop.png)] bg-no-repeat bg-cover pt-[15px] pb-[60px] px-[46px]">
+    <section className="min-w-80 h-[491px] bg-[url('../images/hero-bg-img-mobile.png')] md:bg-[url('../images/hero-bg-img-tablet.png')] xl:bg-[url('../images/hero-bg-img-desktop.png')] bg-no-repeat bg-cover pt-[15px] pb-[60px] px-[46px]">
       <h1 className="text-[#7E6D53] text-5xl font-normal leading-[normal] h-[72px] flex justify-center items-center">
         Nice view
       </h1>
